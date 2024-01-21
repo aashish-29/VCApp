61eefca787116596814cc9a69ac802d17dd005e6
guptash2 <ashish.gupta@msci.com>
2024-01-21 18:54:06 +0530

    minor struc change

---
+++ client/src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1 contenteditable ="true">This is the note title</h1>
+      <p contenteditable ="true">This is the note content</p>
+      <p contenteditable ="true">This is the note content</p>
+    </div>
+  );
+}
+export default Note;
+
